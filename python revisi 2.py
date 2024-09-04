334ba89de29881e49a159006362dce00bd934a9b
nay <145947649+nayllafar@users.noreply.github.com>
2024-09-04 10:50:27 +0700

    Add files via upload

---
+++ python revisi 2.py
@@ -0,0 +1,17 @@
+print("Hello Jempet!")
+print("Belajar Python dari Nol")
+nama = "python pemula"
+print("Hello"); print("Jempet"); print("Tutorial Python untuk Pemula")
+nama_depan = "python";nama_belakang = "pemula"
+"Belajar Pemrograman Python sampai Bisa"penulis = 'python pemula'
+"Belajar Membuat Program Python"
+if username == 'pythonpemula'
+:
+print("Selamat Datang Admin")
+print("Silakan ambil tempat duduk")
+variabel_ku = "ini isi variabel"
+variabel2 = 20
+
+
+
+
